5e8776aac8cc1d03570ad7ca4dadc127944a4c30
R_XIAN <asdzxccxz333@gmail.com>
2020-03-22 18:12:14 +0800

    資料庫轉移

--- connMysqlObj.php
+++ connMysqlObj.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = "us-cdbr-iron-east-04.cleardb.net";
-$db_username = "b188a9e70e9697";
-$db_password = "18c15834";
-$db_name = "heroku_dedfb3243e930ab";
+$db_username = "b586927e7e8dad";
+$db_password = "624a5963";
+$db_name = "heroku_d483171ce082cb3";
 $db_link = @new mysqli($db_host, $db_username, $db_password, $db_name);
 if ($db_link->connect_error != "") {
 	echo "資料庫連結失敗！";
