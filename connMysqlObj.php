43b20b6c3ad3609550ce2b1e4604b273f894a783
R_XIAN <asdzxccxz333@gmail.com>
2020-03-22 16:40:27 +0800

    更改儲存資料的資料庫

--- connMysqlObj.php
+++ connMysqlObj.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name = "mydb";
+$db_host = "us-cdbr-iron-east-04.cleardb.net";
+$db_username = "b188a9e70e9697";
+$db_password = "18c15834";
+$db_name = "heroku_dedfb3243e930ab";
 $db_link = @new mysqli($db_host, $db_username, $db_password, $db_name);
 if ($db_link->connect_error != "") {
 	echo "資料庫連結失敗！";
